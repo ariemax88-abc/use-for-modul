caedeec09afa498343206b1ae12a54f20f9f636d
Ari Bayu Prasetyo <aribprasetyo88@gmail.com>
2022-03-13 13:41:51 +0700

    update modul versi 2

--- for-modul.go
+++ for-modul.go
@@ -2,7 +2,7 @@ package main
 
 //jangan lupa awal huruf func buat Uppercase agar bisa diset public.
 func UseForModul() string {
-	return "Percobaan membuat modul di GoLang"
+	return "Percobaan membuat modul di GoLang versi 2"
 }
 
 func main() {
